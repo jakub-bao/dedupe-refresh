0badd803b4018a0a8b52ee3a5094dbb460864e03
Jakub <jflaska@baosystems.com>
2020-04-01 12:54:25 -0400

    new files

---
+++ testData/users/persistUsers.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import api
+import json
+
+def load_users():
+    file = open("users.json", "r")
+    contents = file.read()
+    return json.loads(contents)
+
+
+def persist_users(users):
+    print(len(users),'users loaded')
+    for user in users:
+        user['userCredentials']['password'] = "Cypress1!"
+        res = api.post('users.json', user)
+        print(res)
+
+
+persist_users(load_users())
