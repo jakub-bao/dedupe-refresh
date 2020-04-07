105e97c53b91f3031df1a0865034923230a20ef1
Jakub <jflaska@baosystems.com>
2020-04-07 10:43:16 -0400

    working

--- src/modules/results/components/resultsTable.component.tsx
+++ src/modules/results/components/resultsTable.component.tsx
@@ -2,37 +2,40 @@ import React from "react";
 import MaterialTable from "material-table";
 import {DedupeModel} from "../models/dedupe.model";
 
+const noSort = {sorting: false};
+const padding = '5px 5px 5px 5px';
+
 const tableOptions = {
     pageSize: 20,
     pageSizeOptions: [20, 50, 100],
     selection: true,
     emptyRowsWhenPaging: false,
     thirdSortClick: false,
-    padding: 'dense' as ('default' | 'dense')
+    padding: 'dense' as ('default' | 'dense'),
+    headerStyle: {padding}
 };
 
-const cellRightBorder = {
-    borderRight: '1px solid black'
-};
-
-function iterateValuesFactory(property:string){
-    return function(dedupe:DedupeModel){
-        return <ol>
-            {dedupe.duplicates.map(duplicate=><li>{duplicate[property]}</li>)}
-        </ol>
-    }
-}
+const borderRight = '1px solid #00000021';
 
 
 const columnSettings = [
-    {title: 'Data Element', field: 'info.dataElementName'},
-    {title: 'Disaggregation', field: 'data.disAggregation'},
-    {title: 'Organisation Unit', field: 'info.orgUnitName', cellStyle:cellRightBorder},
-    {title: 'Agency', render: iterateValuesFactory('agencyName'), sorting: false},
-    {title: 'Partner', render: iterateValuesFactory('partnerName'), sorting: false},
-    {title: 'Mechanism #', render: iterateValuesFactory('mechanismNumber'), sorting: false},
-    {title: 'Value', render: iterateValuesFactory('value'), sorting: false}
+    {title: 'Data Element', field: 'info.dataElementName', cellStyle: {padding}},
+    {title: 'Disaggregation', field: 'data.disAggregation', cellStyle: {padding}},
+    {title: 'Organisation Unit', field: 'info.orgUnitName', cellStyle: {padding, borderRight}},
+    {title: 'Agency', render: iterateValuesFactory('agencyName'), ...noSort, cellStyle: {padding}},
+    {title: 'Partner', render: iterateValuesFactory('partnerName'), ...noSort, cellStyle: {padding}},
+    {title: 'Mechanism #', render: iterateValuesFactory('mechanismNumber'), ...noSort, cellStyle: {padding}},
+    {title: 'Value', render: iterateValuesFactory('value'), ...noSort, cellStyle: {padding, borderRight}}
 ];
+
+
+function iterateValuesFactory(property:string){
+    return function(dedupe:DedupeModel){
+        return <ul style={{paddingInlineStart: 15}}>
+            {dedupe.duplicates.map(duplicate=><li>{duplicate[property]}</li>)}
+        </ul>
+    }
+}
 
 export default function ResultsTable({filteredDedupes}:{filteredDedupes: DedupeModel[]}) {
     return <MaterialTable
